d4a65e1487ea6b471b4948a4645ed44d3f0cfdf5
Unknown <chromeman05@gmail.com>
2018-07-03 14:51:48 -0400

    phase 1 - light mode

---
+++ assets/js/lightmode.js
@@ -0,0 +1,10 @@
+function lightMode()
+{
+        alert("WARNING: You are about to burn your eyes!!!");
+        document.body.style.backgroundColor = '#FFFFFF';
+}
+
+function darkMode()
+{
+    document.body.style.backgroundColor = '#2C2F33';
+}
