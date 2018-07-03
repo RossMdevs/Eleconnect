5eb8ee40f3aa04a4505b7db0be725f24e1d1452c
Unknown <chromeman05@gmail.com>
2018-07-03 19:25:05 -0400

    more updates

--- assets/js/time.js
+++ assets/js/time.js
@@ -3,5 +3,6 @@ var a=setInterval(getTime,1000);
 function getTime(){
     var d = new Date();
 
-    document.getElementById("time").innerHTML = d.getHours() + ':' + d.getMinutes();
+    document.getElementById("time").innerHTML = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 }
+
